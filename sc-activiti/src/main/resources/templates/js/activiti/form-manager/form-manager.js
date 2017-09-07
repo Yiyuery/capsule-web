68763a8a32c9dfa5c572ae06e62b9ca618918ff0
Yiyuery X <xiazhaoyang@live.com>
2017-09-08 00:36:10 +0800

    sc-activiti: 1、html 框架搭建

---
+++ sc-activiti/src/main/resources/templates/js/activiti/form-manager/form-manager.js
@@ -0,0 +1,14 @@
+/**
+ * 在线表单设计
+ * Name     : form-create
+ * Author   : Mitsuha
+ * Date     : 2017-09-03
+ */
+define(["require", "exports", "avalon"], function (require, exports, avalon) {
+    "use strict";
+    exports.__esModule = true;
+    avalon.ready(function () {
+        avalon.scan(document.body);
+    });
+});
+//# sourceMappingURL=form-manager.js.map
